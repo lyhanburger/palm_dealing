087775e91722a2f214c9327847ff951c1bccb92b
liyuhan <liyuhan@megvil.com>
2018-03-03 16:51:05 +0800

    min_rect

--- deal.py
+++ deal.py
@@ -30,8 +30,8 @@ class Predeal:
         # 04 get contours
         contours = self.contours(tresh_img)
 
-        # 05 mask
-
+        # 05 find_min_rec
+        self.find_min_rec(img, contours)
         # draw images
         # opencv的一个像素为：[B,G,R] ,matplotlib的一个像素为：[R,G,B]
         plt.figure()
@@ -69,7 +69,7 @@ class Predeal:
         #  gray_img
         #  plt.hist(img_gray.ravel(), 256)
         #  plt.show()
-        retval, im_at_fixed = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU)
+        retval, im_at_fixed = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
         return im_at_fixed
 
@@ -83,8 +83,8 @@ class Predeal:
             return cv2.filter2D(img, -1, kernel)
         elif choice == 3:
             return cv2.GaussianBlur(img, tmp, 0)
-
-
+        elif choice == 4:
+            kernel = np.ones(tmp, np.float32)
 
 
     def contours(self, img):
@@ -94,6 +94,29 @@ class Predeal:
 
         image, contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         return contours
+
+    def find_min_rec(self, img, contours):
+        for p in contours:
+            x, y, w, h = cv2.boundingRect(p)
+            cv2.rectangle(img, (x, y), (x+w , y+h), (0, 255, 0), 2)
+            # find minimum area
+            rect = cv2.minAreaRect(p)
+            # calculate coordinates of the minimum area rectangle
+            box = cv2.boxPoints(rect)
+            # normalize coordinates to integers
+            box = np.int0(box)
+            # draw contours
+            cv2.drawContours(img, [box], 0, (0, 0, 255), 3)
+
+            # calculate center and radius of minimum enclosing circle
+            (x, y), radius = cv2.minEnclosingCircle(p)
+            # cast to integers
+            center = (int(x), int(y))
+            radius = int(radius)
+            # draw the circle
+            img = cv2.circle(img, center, radius, (0, 255, 0), 2)
+
+        cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
 
 
 if __name__ == '__main__':
