fd9f561928a3574d9fdf8db284a40c4131b8cd87
lihao <lihao3016@gmail.com>
2018-03-11 17:56:48 +0800

    wechat

--- wechat/pages/index/index.js
+++ wechat/pages/index/index.js
@@ -1,23 +1,64 @@
-1//index.js  
+//index.js  
 //获取应用实例  
 var app = getApp()
 Page({
   data: {
-    tempFilePaths: ''
+    tempFilePaths: '',
+    content:"https://www.lihao7086.com/download"
   },
-  onLoad: function () {
+onLoad: function () {
+	wx.chooseImage({
+	  success: function(res) {
+	    var tempFilePaths = res.tempFilePaths
+	    wx.uploadFile({
+	      url: 'https://www.lihao7086.com/upload', //仅为示例，非真实的接口地址
+	      filePath: tempFilePaths[0],
+	      name: 'file',
+	      formData:{
+	        'user': 'test'
+	      },
+	      success: function(res){
+	        var data = res.data
+			wx.previewImage({
+			  current: data, // 当前显示图片的http链接
+			  urls: [data] // 需要预览的图片http链接列表
+			})
+			console.log(res.data)
+			
+	      }
+	    })
+	  }
+	})
   },
+	downLoad: function(){
+			wx.downloadFile({
+					url: 'https://www.lihao7086.com/download', //仅为示例，并非真实的资源
+	  				success: function(res) {
+					    if (res.statusCode === 200) {
+							wx.previewImage({
+							  current: res.tempFilePath, // 当前显示图片的http链接
+							  urls: [res.tempFilePath] // 需要预览的图片http链接列表
+							})
+	   					 }
+	  				}
+			})
+	},
+
   chooseimage: function () {
     var _this = this;
     wx.chooseImage({
-      count: 2, // 默认9  
+      count: 1, // 默认9  
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
         _this.setData({
-          tempFilePaths: res.tempFilePaths
+          tempFilePaths: res.tempFilePaths,
+          content:"success"
         })
+      },
+      fail:function(res){
+        content:"fails"
       }
     })
   }
