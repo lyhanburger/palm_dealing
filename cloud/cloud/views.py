7a81ed14dd1b375752d1e7505dec1756f4cbae47
lihao2333 <lihao2333@gmail.com>
2018-03-13 17:03:11 +0800

    能够实现小程序拍照返回轮廓

--- cloud/cloud/views.py
+++ cloud/cloud/views.py
@@ -3,20 +3,23 @@ from django.shortcuts import render
 import os
 from django.views.decorators.csrf import csrf_exempt
 import cv2
-from opencv.test import test
+import opencv.cutout.cutout as cutout
+import opencv.roi.roi as roi
+import opencv.feature.seven.feature_7 as feature_7
 @csrf_exempt
 def upload(request):
 	print("hh")
 	if request.method == "POST":
 		obj = request.FILES.get("file")
 		obj_path = os.path.join('static', obj.name)
-		obj_path_prefix = obj_path.rstrip(".jpeg")
+		obj_path_prefix = obj_path.rstrip(".jpg").rstrip(".jpeg").rstrip(".png")
 		f = open(obj_path, 'wb')
 		for line in obj.chunks():
 			f.write(line)
 		f.close()
-		img = cv2.imread(obj_path)
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		cv2.imwrite(obj_path_prefix+"_out.jpeg", img)
-
-		return HttpResponse("https://www.lihao7086.com/"+obj_path_prefix+"_out.jpeg")
+#		img = cv2.imread(obj_path)
+#		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#		cv2.imwrite(obj_path_prefix+"_out.jpeg", img)
+		cutout.cutout(obj_path)
+		print(obj_path)
+		return HttpResponse("https://www.lihao7086.com/"+obj_path_prefix+"_contour.jpg")
