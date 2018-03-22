ed5b19d76a47e139ae94404d14abb6a133dda57e
lihao <lihao3016@gmail.com>
2018-03-22 21:32:06 +0800

    wechat_report

--- wechat/pages/index/index.js
+++ wechat/pages/index/index.js
@@ -1,6 +1,40 @@
 //index.js  
 //获取应用实例  
 var app = getApp()
+App({
+  onLaunch: function() {
+    wx.login({
+		success:function(res){
+			console.log("ok")	
+			wx.getSetting({
+			    success(res) {
+			        if (!res.authSetting['scope.record']) {
+			            wx.authorize({
+			                scope: 'scope.record',
+			                success() {
+			                    // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+			                    wx.startRecord()
+			                }
+			            })
+			        }
+			    }
+			})
+			wx.getUserInfo({
+			  success: function(res) {
+			    var userInfo = res.userInfo
+			    var nickName = userInfo.nickName
+			    var avatarUrl = userInfo.avatarUrl
+			    var gender = userInfo.gender //性别 0：未知、1：男、2：女
+			    var province = userInfo.province
+			    var city = userInfo.city
+			    var country = userInfo.country
+				console.log(userInfo)
+			  }
+			})
+		}	
+	});
+  }
+})
 Page({
   data: {
     tempFilePaths: '',
@@ -8,11 +42,13 @@ Page({
   },
 onLoad: function(){},
 upLoad: function () {
+	var _this = this
 	wx.chooseImage({
 			count:1,
 			sizeType:['original'],
 	  success: function(res) {
 	    var tempFilePaths = res.tempFilePaths
+		_this.setData({content:"正在上传,大约需要30s,请不要操作其他"})
 	    wx.uploadFile({
 	      url: 'https://www.lihao7086.com/upload', //仅为示例，非真实的接口地址
 	      filePath: tempFilePaths[0],
@@ -20,50 +56,53 @@ upLoad: function () {
 	      formData:{
 	        'user': 'test'
 	      },
+//--------------upload success
 	      success: function(res){
-	        var data = res.data
-			var murls = data.split(",")
-			console.log(res.data)
-			console.log("murls is "+murls)
-			console.log("murls[0] is "+murls[0])
-			wx.previewImage({
-			  current: murls[0], // 当前显示图片的http链接
-			  urls:[murls[0],murls[1]] // 需要预览的图片http链接列表
-			})
+	        var data = res.data//get url
+//			-------download pdf by url
+					wx.downloadFile({
+					  url: data,
+					  success: function (res) {
+					    var filePath = res.tempFilePath
+						_this.setData({content:""})
+					    wx.openDocument({
+					      filePath: filePath,
+					      success: function (res) {
+					        console.log('打开文档成功')
+					      }
+					    })
+					  }
+					})
 			
 	      }
 	    })
 	  }
 	})
   },
-	downLoad: function(){
-			wx.downloadFile({
-					url: 'https://www.lihao7086.com/download', //仅为示例，并非真实的资源
-	  				success: function(res) {
-					    if (res.statusCode === 200) {
-							wx.previewImage({
-							  current: res.tempFilePath, // 当前显示图片的http链接
-							  urls: [res.tempFilePath] // 需要预览的图片http链接列表
-							})
-	   					 }
-	  				}
-			})
-	},
-
-  chooseimage: function () {
-    var _this = this;
-    wx.chooseImage({
-      count: 1, // 默认9  
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
-        _this.setData({
-          tempFilePaths: res.tempFilePaths,
-        })
-      },
-      fail:function(res){
-      }
-    })
-  }
+upLoadDemo_fast:function(){
+	var _this = this
+	var tempcontent = "正在上传示例图片,大约需要30s,请不要操作其他"
+	this.setData({content:tempcontent})
+	wx.downloadFile({url:'https://www.lihao7086.com/static/demo/yangxuanyue_report.pdf',
+		success:function(res){
+			var filePath = res.tempFilePath
+			_this.setData({content:""})
+			wx.openDocument({filePath:filePath,success:function(res){console.log('open success')}})
+		}})	
+},
+upLoadDemo: function(){
+	wx.downloadFile({url:'https://www.lihao7086.com/static/demo/yangxuanyue.jpg',
+		success:function(res){
+			var imgpath = res.tempFilePath
+			wx.uploadFile({url:'https://www.lihao7086.com/upload',filePath:imgpath,name:'file',formData:{'user':'test'},
+				success:function(res){
+					var data = res.data
+					wx.downloadFile({url:data,
+						success:function(res){
+							var filePath = res.tempFilePath
+									wx.openDocument({filePath:filePath,success:function(res){console.log('打开文档成功')}})
+							}})
+				}})
+		}})
+}
 })  
