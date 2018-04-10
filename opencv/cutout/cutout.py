dd11ee1843e4580a0e6b8f750effbe9f586eb0ce
lihao <lihao3016@gmail.com>
2018-04-10 10:47:47 +0800

    edit_cutout

--- opencv/cutout/cutout.py
+++ opencv/cutout/cutout.py
@@ -11,14 +11,20 @@ def cutout(img_file):
 #    #lower = np.array([0,139,70], np.uint8)#lihao
 #    upper = np.array([255,180,127], np.uint8)
 # new
-    lower = np.array([0,137,100], np.uint8)#yangxuanyue
-    upper = np.array([255,175,118], np.uint8)
+    lower = np.array([0,131,102], np.uint8)#yangxuanyue
+    upper = np.array([255,183,154], np.uint8)
 	
     ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
     mask = cv2.inRange(ycrcb, lower, upper)
-    skin = cv2.bitwise_and(img, img, mask=mask)
+#    skin = cv2.bitwise_and(img, img, mask=mask)
+#    cv2.imwrite("{0}_skin.jpg".format(IMAGE_USER),skin)
+
+    kernel = np.ones((57,57),np.uint8)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    skin = cv2.bitwise_and(img, img, mask=opening)
     cv2.imwrite("{0}_skin.jpg".format(IMAGE_USER),skin)
-    _,black_and_white = cv2.threshold(mask, 27,255,0)
+
+    _,black_and_white = cv2.threshold(opening, 127,255,0)
     #find max contours
     _, contours, _ = cv2.findContours(black_and_white, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     length = len(contours)
@@ -38,4 +44,5 @@ def cutout(img_file):
     cv2.imwrite("{0}_contour_skin.jpg".format(IMAGE_USER),skin)
 
 if __name__ == '__main__':
-    cutout("../image/yangxuanyue/yangxuanyue.jpg")
+#    cutout("../image/yangxuanyue/yangxuanyue.jpg")
+    cutout("../image/lihao/lihao.jpg")
