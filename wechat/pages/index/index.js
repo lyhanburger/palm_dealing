d37d9b47cebbcfb93c8526db9f87c4a612f041c3
lihao <lihao3016@gmail.com>
2018-04-10 11:53:33 +0800

    wechat 整理代码

--- wechat/pages/index/index.js
+++ wechat/pages/index/index.js
@@ -3,36 +3,36 @@
 var app = getApp()
 App({
   onLaunch: function() {
-    wx.login({
-		success:function(res){
-			console.log("ok")	
-			wx.getSetting({
-			    success(res) {
-			        if (!res.authSetting['scope.record']) {
-			            wx.authorize({
-			                scope: 'scope.record',
-			                success() {
-			                    // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
-			                    wx.startRecord()
-			                }
-			            })
-			        }
-			    }
-			})
-			wx.getUserInfo({
-			  success: function(res) {
-			    var userInfo = res.userInfo
-			    var nickName = userInfo.nickName
-			    var avatarUrl = userInfo.avatarUrl
-			    var gender = userInfo.gender //性别 0：未知、1：男、2：女
-			    var province = userInfo.province
-			    var city = userInfo.city
-			    var country = userInfo.country
-				console.log(userInfo)
-			  }
-			})
-		}	
-	});
+//    wx.login({
+//		success:function(res){
+//			console.log("ok")	
+//			wx.getSetting({
+//			    success(res) {
+//			        if (!res.authSetting['scope.record']) {
+//			            wx.authorize({
+//			                scope: 'scope.record',
+//			                success() {
+//			                    // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+//			                    wx.startRecord()
+//			                }
+//			            })
+//			        }
+//			    }
+//			})
+//			wx.getUserInfo({
+//			  success: function(res) {
+//			    var userInfo = res.userInfo
+//			    var nickName = userInfo.nickName
+//			    var avatarUrl = userInfo.avatarUrl
+//			    var gender = userInfo.gender //性别 0：未知、1：男、2：女
+//			    var province = userInfo.province
+//			    var city = userInfo.city
+//			    var country = userInfo.country
+//				console.log(userInfo)
+//			  }
+//			})
+//		}	
+//	});
   }
 })
 Page({
@@ -44,41 +44,39 @@ onLoad: function(){},
 upLoad: function () {
 	var _this = this
 	wx.chooseImage({
-			count:1,
-			sizeType:['original'],
-	  success: function(res) {
-	    var tempFilePaths = res.tempFilePaths
-		_this.setData({content:"正在上传,大约需要30s,请不要操作其他"})
-	    wx.uploadFile({
-	      url: 'https://www.lihao7086.com/upload', //仅为示例，非真实的接口地址
-	      filePath: tempFilePaths[0],
-	      name: 'file',
-	      formData:{
-	        'user': 'test'
-	      },
-//--------------upload success
-	      success: function(res){
-	        var data = res.data//get url
-//			-------download pdf by url
-					wx.downloadFile({
-					  url: data,
-					  success: function (res) {
-					    var filePath = res.tempFilePath
-						_this.setData({content:""})
-					    wx.openDocument({
-					      filePath: filePath,
-					      success: function (res) {
-					        console.log('打开文档成功')
-					      }
-					    })
-					  }
-					})
-			
-	      }
-	    })
-	  }
+		count:1,
+		sizeType:['original'],
+		success: function(res) {
+			var tempFilePaths = res.tempFilePaths
+		 	_this.setData({content:"正在上传,大约需要30s,请不要操作其他"})
+			wx.uploadFile({
+	  			url: 'https://www.lihao7086.com/upload', //仅为示例，非真实的接口地址
+	  			filePath: tempFilePaths[0],
+	  			name: 'file',
+	  			formData:{ 'user': 'test'},
+			   	success: function(res){
+					console.log('uploadFileSuccess')
+			     	var data = res.data//get url
+			  		wx.downloadFile({
+			  			url: data,
+			  			success: function (res) {
+			  				var filePath = res.tempFilePath
+			  				_this.setData({content:""})
+			  			    wx.openDocument({
+			  			   		filePath: filePath,
+			  			     	success: function (res) {console.log('打开文档成功')}
+			  			    })
+			  			  }
+			  		})
+			  	
+			   	},
+				fail:function(res){
+					console.log('uploadFile fail')
+				}
+			})
+		},
 	})
-  },
+},
 upLoadDemo_fast:function(){
 	var _this = this
 	var tempcontent = "正在上传示例图片,大约需要30s,请不要操作其他"
